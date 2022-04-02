8f865e0b6fc390ae5922203e58344e905d73c316
Omprakash sahu <subassahu72@gmail.com>
2022-04-02 15:45:59 +0530

    added imlementation for multiply.py

--- multiply.py
+++ multiply.py
@@ -2,5 +2,13 @@ def multiply(a,b):
     c = a * b
     return c
 
-total = multiply(5,8)
-print(total)
+mul = multiply(5,8)
+print(mul)
+
+
+def substraction(a,b):
+    c = a - b
+    return c
+
+sub = substraction(23,45)
+print(sub)
