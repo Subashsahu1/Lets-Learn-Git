2a8cda913688c6430f1d20f3dec047be278c2d4e
Omprakash sahu <subassahu72@gmail.com>
2022-04-02 12:39:59 +0530

    Initial commit

---
+++ sum.py
@@ -0,0 +1,10 @@
+class Sum:
+    def __init__(self,a,b):
+        self.firstNo = a
+        self.secondNo = b
+
+    def addTwoNo(self):
+        print(f"Sum of {self.firstNo} and {self.secondNo} is {self.firstNo + self.secondNo}.")
+
+subash = Sum(10,20)
+subash.addTwoNo()
